975327f37b106b37f5c2c02447c243c428a585b6
lihy <lihy@fenbi.com>
2016-02-03 18:36:09 +0800

    ADD: Crypto sha256 by file content

--- Crypto.cxx
+++ Crypto.cxx
@@ -17,17 +17,51 @@ bool Crypto::sha256(const vector<char> &input, vector<char>& output) {
 bool Crypto::sha256Hex(const string &input, string& output) {
     // make vec input
     vector<char> inputVec;
-    std::copy( input.begin(), input.end(), std::back_inserter(inputVec));
+    if(!stringToVec(input, inputVec)) {
+        return false;
+    }
     // sha256(vec)
     vector<char> outputVec;
     if(!sha256(inputVec, outputVec)) {
         return false;
     }
     // debug
+    return digestToHex(outputVec, output);
+}
+
+bool Crypto::stringToVec(const string& str, vector<char>& vec) {
+    std::copy( str.begin(), str.end(), std::back_inserter(vec));
+    return true;
+}
+
+bool Crypto::digestToHex(const vector<char>& input, string& output) {
     output.clear();
     CryptoPP::HexEncoder encoder;
     encoder.Attach( new CryptoPP::StringSink( output ) );
-    encoder.Put( reinterpret_cast<const byte*>(&outputVec[0]), sizeof(char)*outputVec.size() );
+    encoder.Put( reinterpret_cast<const byte*>(&input[0]), sizeof(char)*input.size() );
     encoder.MessageEnd();
     return true;
+}
+
+bool Crypto::sha256ByFile(const string& fileName, vector<char>& output) {
+    // Open & load file 
+    FILE* fp = fopen(fileName.c_str(), "rb");
+    if(!fp) {
+        return false;
+    }
+    vector<char> input;
+    char tmp;
+    while(1 == fread(&tmp, sizeof(char), 1, fp)) {
+        input.push_back(tmp);
+    }
+    fclose(fp);
+    return sha256(input, output);
+}
+
+bool Crypto::sha256HexByFile(const string& fileName, string& output) {
+    vector<char> outputVec;
+    if(!sha256ByFile(fileName, outputVec)) {
+        return false;
+    }
+    return digestToHex(outputVec, output);
 }
--- Crypto.h
+++ Crypto.h
@@ -12,7 +12,13 @@ class Crypto {
         // sha256 hash
         static bool sha256(const vector<char> &input, vector<char>& output);
         static bool sha256Hex(const string &input, string& output);
+        static bool sha256ByFile(const string& fileName, vector<char>& output);
+        static bool sha256HexByFile(const string& fileName, string& output);
         // aes
+
+        // util
+        static bool stringToVec(const string& str, vector<char>& vec);
+        static bool digestToHex(const vector<char>& input, string& output);
 }
 ;
 
